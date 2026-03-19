33ca0ebf3fe2dd667ce3c210feb943542ef162b6
Mark Manders <mmjmanders@gmail.com>
2026-03-19 11:27:09 +0100

    Added PWA capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,36 @@ export default defineConfig({
       },
       registerType: 'autoUpdate',
       strategies: 'generateSW',
+      manifest: {
+        name: "Conway's Game of Life",
+        short_name: 'Game of Life',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
     }),
     vueDevTools(),
   ],
