668c8e50f7aada215b4664c450dc730252f4931f
Mark Manders <mmjmanders@gmail.com>
2026-03-19 11:14:47 +0100

    Added tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import './main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
-
-app.mount('#app')
+app.use(createPinia()).mount('#app')
