2d2de066a64b4675c5535138742b6432c5e05349
Mark Manders <mmjmanders@gmail.com>
2026-03-24 09:18:36 +0100

    Set up GitHub Actions workflow for GitHub Pages deployment and configure environment-based base path in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const { BASE_PATH } = process.env
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: BASE_PATH || '/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
