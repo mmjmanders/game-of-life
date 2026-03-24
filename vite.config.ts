40cef63c5bb7d3d5295088df445585e90e93770b
Mark Manders <mmjmanders@gmail.com>
2026-03-24 09:50:08 +0100

    Feature/add pwa and node (#2)
    * Added svg icon
    
    * Integrate PWA support with `vite-plugin-pwa` and configure manifests.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 const { BASE_PATH } = process.env
@@ -10,7 +11,48 @@ const { BASE_PATH } = process.env
 // https://vite.dev/config/
 export default defineConfig({
   base: BASE_PATH || '/',
-  plugins: [vue(), tailwindcss(), vueDevTools()],
+  plugins: [
+    vue(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: 'autoUpdate',
+      strategies: 'generateSW',
+      manifest: {
+        name: "Conway's Game of Life",
+        short_name: 'Game of Life',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+    tailwindcss(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
