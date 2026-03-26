15eb8a7f4d7363177f7f9eebb6f2b8fbe33f356a
Mark Manders <mmjmanders@gmail.com>
2026-03-26 14:37:40 +0100

    Add GitHub link in header and FontAwesome brand icons integration
    - Display GitHub link dynamically in the header using `VITE_GITHUB_REPOSITORY_URL` with version display.
    - Include FontAwesome GitHub brand icon in the project.
    - Update dependencies to add `@fortawesome/free-brands-svg-icons`.
    - Adjust GitHub Pages workflow to set `VITE_GITHUB_REPOSITORY_URL` during the build process.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import GameOfLife from '@/components/GameOfLife.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-const { VITE_APP_VERSION: version } = import.meta.env
+const { VITE_APP_VERSION: version, VITE_GITHUB_REPOSITORY_URL: url } = import.meta.env
 </script>
 
 <template>
   <header>
     <h1>
       Conway's Game of Life
-      <span class="version">{{ version || 'dev' }}</span>
+      <a v-if="version && url" class="version" :href="url" target="_blank" rel="noopener">
+        <FontAwesomeIcon :icon="['fab', 'github']" />
+        {{ version }}
+      </a>
     </h1>
   </header>
   <main>
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faForwardStep, faPlay, faStop } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faPlay, faStop, faForwardStep)
+library.add(faPlay, faStop, faForwardStep, faGithub)
 
 createApp(App).use(createPinia()).mount('#app')
